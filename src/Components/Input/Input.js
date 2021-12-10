326ca58c214e93e4c2d54367dbf7378949d1e25e
Taras Boikov <tarasgowest@gmail.com>
2021-12-11 00:29:12 +0200

    добавляет инпут

--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -1,12 +1,22 @@
-const Input = () => {
+const Input = ({ name, AddContactName, AddContact }) => {
   return (
-    <input
-      type="text"
-      name="name"
-      pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-      title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-      required
-    />
+    <form>
+      <h3>Name</h3>
+      <label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          onChange={AddContactName}
+        />
+      </label>
+      <button type="button" onClick={AddContact}>
+        Add contact
+      </button>
+    </form>
   );
 };
 
