700f0ba4e8aca3f20fad2c42bfa520667610eabc
Taras Boikov <tarasgowest@gmail.com>
2021-12-11 00:29:32 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+
 import Input from './Components/Input/Input';
 import Title from './Components/Title/Title';
 
-function App() {
-  // state = {
-  //   contacts: [],
-  //   name: ''
-  // };
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
 
-  return (
-    <div>
-      <Title title="Phonebook" />
-      <Input />
-      <Title title="Contacts" />
-    </div>
-  );
+  AddContactName = event => {
+    this.setState({ name: event.currentTarget.value });
+  };
+
+  AddContact = event => {
+    this.setState(prevState => ({
+      contacts: (prevState.contacts += this.state.name),
+    }));
+  };
+
+  render() {
+    const { contacts } = this.state;
+    const { name } = this.state;
+    return (
+      <div>
+        <Title title="Phonebook" />
+        <Input
+          name={name}
+          AddContactName={this.AddContactName}
+          AddContact={this.AddContact}
+        />
+        <Title title="Contacts" />
+      </div>
+    );
+  }
 }
 
 export default App;
