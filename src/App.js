65c516209d5525ad1bd093f15884eec782b55697
Taras Boikov <tarasgowest@gmail.com>
2021-12-11 23:24:45 +0200

    добавляет рендер контактов

--- src/App.js
+++ src/App.js
@@ -1,37 +1,68 @@
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
 import React, { Component } from 'react';
 
 import Input from './Components/Input/Input';
 import Title from './Components/Title/Title';
+import Contacts from './Components/Contacts/Contacts';
+import Filter from './Components/Filter/Filter';
+import { nanoid } from 'nanoid';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
     name: '',
+    number: '',
   };
 
   AddContactName = event => {
     this.setState({ name: event.currentTarget.value });
   };
 
+  AddContactNumber = event => {
+    this.setState({ number: event.currentTarget.value });
+  };
+
   AddContact = event => {
     this.setState(prevState => ({
-      contacts: (prevState.contacts += this.state.name),
+      contacts: [...prevState.contacts, this.RenderContact()],
+      name: '',
+      number: '',
     }));
   };
 
+  RenderContact = () => {
+    return { id: nanoid(), name: this.state.name, number: this.state.number };
+  };
+
+  FilterContact = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
   render() {
-    const { contacts } = this.state;
-    const { name } = this.state;
+    const { contacts, filter, name, number } = this.state;
+    const normFilter = this.state.filter;
+    const filteredContacts = this.state.contacts.name.filter(names =>
+      names.text.includes(normFilter),
+    );
     return (
       <div>
         <Title title="Phonebook" />
         <Input
           name={name}
+          number={number}
           AddContactName={this.AddContactName}
+          AddContactNumber={this.AddContactNumber}
           AddContact={this.AddContact}
         />
         <Title title="Contacts" />
+        <Filter value={filter} onChange={this.FilterContact} />
+        <Contacts contacts={contacts} />
       </div>
     );
   }
--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -1,4 +1,10 @@
-const Input = ({ name, AddContactName, AddContact }) => {
+const Input = ({
+  name,
+  number,
+  AddContactName,
+  AddContactNumber,
+  AddContact,
+}) => {
   return (
     <form>
       <h3>Name</h3>
@@ -11,6 +17,16 @@ const Input = ({ name, AddContactName, AddContact }) => {
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           onChange={AddContactName}
+        />
+        <h3>Number</h3>
+        <input
+          type="tel"
+          name="number"
+          value={number}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          onChange={AddContactNumber}
         />
       </label>
       <button type="button" onClick={AddContact}>
